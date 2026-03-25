f888924c97349a414fc4b45afee84b7cd121f3b3
Ayushanupam7 <ayushanupamofficial7@gmail.com>
2026-03-26 02:11:59 +0530

    chore: prepare for Vercel deployment (requirements.txt, vercel.json, dynamic API URL)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import ChatBox from "./components/ChatBox";
 import ChatInput from "./components/ChatInput";
 import LogoutModal from "./components/LogoutModal";
 
-const API = "http://127.0.0.1:8000";
+const API = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 function App() {
   const [messages, setMessages] = useState([]);
