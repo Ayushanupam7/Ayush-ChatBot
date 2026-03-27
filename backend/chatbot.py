45d4b792800cce540bfce7906fc748e2b952248a
Ayushanupam7 <ayushanupamofficial7@gmail.com>
2026-03-27 16:40:55 +0530

    modified:   backend/chatbot.py

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -269,7 +269,7 @@ def chat(user: UserInput):
 def greet(name: str = "User"):
     hour = datetime.datetime.now().hour
     if 5 <= hour < 12:
-        g = "Good Morning"
+        g = "Good ayush"
     elif 12 <= hour < 17:
         g = "Good Afternoon"
     elif 17 <= hour < 21:
